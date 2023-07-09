158c0fe90246c3bd1b9cd6bfaa29bb6a74ad339b
Sean Kozen <sgkozen@gmail.com>
2023-07-09 15:31:59 +0900

    Reveal on scroll

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -127,14 +127,11 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 // observer.observe(section1);
 
 const navHeight = nav.getBoundingClientRect().height;
-console.log(navHeight);
+
 
 
 const stickyNav = function(entries) {
   const [entry] = entries;
-  
-  console.log(entry);
-
   if(!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 };
@@ -147,8 +144,31 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 
 headerObserver.observe(header);
 
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = (entries, observer) => {
+  const [entry] = entries;
+  console.log(entry);
+
+  if(!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  //Unobserve sections
+  observer.unobserve(entry.target);
+  
 
 
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+})
+
+allSections.forEach(function(section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 
 
 
