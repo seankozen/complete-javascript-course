45a2f95a8f9b362868a0f788d9abd0b485cbe95f
Sean Kozen <sgkozen@gmail.com>
2023-07-17 13:17:21 +0900

    DOM events

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -170,8 +170,6 @@ const imgTargets = document.querySelectorAll('img[data-src]');
 
 const loadImg = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
-
   if (!entry.isIntersecting) return;
   //Replace source attribute
 
@@ -267,7 +265,6 @@ const slider = function () {
 
   dotContainer.addEventListener('click', function (e) {
     if (e.target.classList.contains('dots__dot')) {
-      console.log('DOT');
       const { slide } = e.target.dataset;
       goToSlide(slide);
       activateDot(slide);
@@ -276,169 +273,22 @@ const slider = function () {
 };
 
 slider();
+
+
+document.addEventListener('DOMContentLoaded', function(e){
+  console.log('HTML parsed and DOM tree built.', e);
+});
+
+window.addEventListener('load', function(e){
+  console.log('Page fully loaded!', e);
+});
+
+// window.addEventListener('beforeunload', function(e){
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
+
 ////////////////////////////////////
 ////////////////////////////////////
 
-// console.log(document.documentElement);
-// const allSections = document.querySelectorAll('.section');
-// console.log(allSections);
-
-// document.getElementById('section--1');
-// const allButtons = document.getElementsByTagName('button');
-// console.log(allButtons);
-
-// console.log(document.getElementsByClassName('btn'));
-
-// const header = document.querySelector('.header');
-
-// // Creating and Inserting Elements
-// //.insertAdjacentHTML
-
-// const message = document.createElement('div');
-// message.classList.add('cookie-message');
-// message.innerHTML =
-//   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
-
-//   header.prepend(message);
-//   //header.append(message);  //Cannot be in two places at once
-//   //header.append(message.cloneNode(true));
-
-//   //header.before(message);
-//   //header.after(message);
-
-//   document.querySelector('.btn--close-cookie').addEventListener('click', () =>{
-//     message.remove();
-//   });
-
-// //Styles
-// message.style.backgroundColor = '#37383d';
-// message.style.width ='120%';
-
-// console.log(message.style.backgroundColor); //works only for inline sytles
-// console.log(getComputedStyle(message).height);
-
-// //Change height
-// message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
-
-// //Change CSS root variables
-// document.documentElement.style.setProperty('--color-primary', 'orangered');
-
-// //Attributes
-// const logo = document.querySelector('.nav__logo');
-// console.log(logo.alt);
-// console.log(logo.src);
-
-// console.log(logo.src);
-// console.log(logo.getAttribute('src')); //gets relative path
-
-// // Data attributes
-// console.log(logo.dataset.versionNumber);
-
-// logo.classList.add('c');
-// logo.classList.remove('c', 'j');
-// logo.classList.toggle('c');
-// logo.classList.contains('c');
-
-// //Don't use...will erase all other classes
-// logo.className = 'sean';
-
-// const btnScrollTo = document.querySelector('.btn--scroll-to');
-// const section1 = document.querySelector('#section--1');
-
-// btnScrollTo.addEventListener('click', e => {
-//   const s1coords = section1.getBoundingClientRect();
-//   console.log(s1coords);
-//   console.log(e.target.getBoundingClientRect());
-//   console.log('Current scroll (X/Y):', window.scrollX, scrollY);
-//   console.log(
-//     'height/width of viewport:',
-//     document.documentElement.clientHeight,
-//     document.documentElement.clientWidth
-//   );
-
-//   Scrolling
-//   Top needs to be relative to top of whole page
-//   window.scrollTo(s1coords.left + window.scrollX, s1coords.top + window.scrollY);
-
-//   window.scrollTo({
-//     left: s1coords.left + window.scrollX,
-//     top: s1coords.top + window.scrollY,
-//     behavior: 'smooth',
-//   })
-
-//   Modern method
-//   section1.scrollIntoView({ behavior: 'smooth' });
-// });
-
-// const h1 = document.querySelector('h1');
-
-// const alertH1 = function (e) {
-//   alert('addEventListener: You are reading the heading!');
-
-//   //remove event listener
-//   h1.removeEventListener('mouseenter', alertH1);
-// };
-
-// h1.addEventListener('mouseenter', alertH1);
-
-// //Older way to add event listeners
-// h1.onmouseenter = e => {
-//   alert('onmouseenter: You are reading the heading!');
-// };
-
-// //Event propogation
-// //rgb(255,255,255)
-// const randomInt = (min, max) =>
-//   Math.floor(Math.random() * (max - min + 1) + min);
-// const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
-// console.log(randomColor());
-
-// document.querySelector('.nav__link').addEventListener('click', function (e) {
-//   this.style.backgroundColor = randomColor();
-//   console.log('LINK', e.target, e.currentTarget);
-//   console.log(e.currentTarget === this);
-
-//   //Stop propogation (Usually not a good thing to do)
-//   // e.stopPropagation();
-
-// });
-
-// document.querySelector('.nav__links').addEventListener('click', function (e) {
-//   this.style.backgroundColor = randomColor();
-//   console.log('CONTAINER', e.target,e.currentTarget);
-// });
-
-// document.querySelector('.nav').addEventListener('click', function (e) {
-//   this.style.backgroundColor = randomColor();
-//   console.log('NAV', e.target,e.currentTarget);
-// });
-
-//DOM Traversing
-
-// const h1 = document.querySelector('h1');
-
-// //Going downwards
-// console.log(h1.querySelectorAll('.highlight'));
-// console.log(h1.childNodes);
-// console.log(h1.children); //Limits nodes to HTML elements that are direct children
-// h1.firstElementChild.style.color = 'white';
-// h1.lastElementChild.style.color = 'orangered';
-
-// //Going upwards
-// console.log(h1.parentNode);
-// console.log(h1.parentElement);
-
-// //Finds parents
-// h1.closest('.header').style.background = 'var(--gradient-secondary)';
-
-// //Going sideways: siblings
-// console.log(h1.previousElementSibling);
-// console.log(h1.nextElementSibling);
-
-// console.log(h1.previousSibling);
-// console.log(h1.nextSibling);
-
-// console.log(h1.parentElement.children);
-// [...h1.parentElement.children].forEach((el) =>{
-//   if(el !== h1)el.style.transform = 'scale(0.5)';
-// });
